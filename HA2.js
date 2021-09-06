04e50cb60ee46a480d1dc175c80f48a2d4b1ce6d
Kate Isaacs <kisaacs@cs.arizona.edu>
2021-09-06 14:22:36 -0700

    Base files for HA2.

---
+++ HA2.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function () {
+    
+    createBarChart('drawing0', grade_percents);
+
+
+    // YOUR CODE GOES HERE
+});
+
+// Create the Bar Chart view
+// elm - the name of the SVG element housing the view
+// data - the passed in dataset (the object "grade_percents" in this case)
+let createBarChart = function(elm, data) {
+
+    // YOUR CODE HERE
+
+
+
+};
+
+
+
+
+// MORE OF YOUR CODE GOES HERE FOR SCATTER 
+
+
+
+
